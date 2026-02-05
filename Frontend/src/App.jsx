88844226007e0e80bcc106b85a7198ccf541128e
MSG24x7 <MSG24x7@example.com>
2026-02-05 13:32:11 +0530

    updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,6 @@
-import React from 'react'
+
 import Nav from './Components/Nav.jsx'
 import Footer from './Components/Footer.jsx'
-import Hero from './Pages/Hero.jsx'
-import About from './Pages/about.jsx'
-import Service from './Pages/Service.jsx'
-import Whychoose from './Pages/Whychoose.jsx'
-import Work from './Pages/Work.jsx'
-import Review from './Pages/Review.jsx'
-import Contact from './Pages/contact.jsx'
 import Home from './Pages/Home.jsx'
 
 const App = () => {
